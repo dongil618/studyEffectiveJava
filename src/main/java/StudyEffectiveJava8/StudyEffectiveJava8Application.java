1e23bf1b27da5959d88e9c367d8e949c59eab3bc
dongil618 <rlaehddlf618@gmail.com>
2024-01-05 22:09:09 +0900

    chore : first commit
    java 8 버전 세팅

---
+++ src/main/java/StudyEffectiveJava8/StudyEffectiveJava8Application.java
@@ -0,0 +1,13 @@
+package StudyEffectiveJava8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyEffectiveJava8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyEffectiveJava8Application.class, args);
+    }
+
+}
